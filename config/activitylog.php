eaac44454a26d4974f506972fc09cea72d529ffc
Sadman Rafid <amsrafid@gmail.com>
2021-03-15 12:54:53 +0600

    Create console to clear log

--- config/activitylog.php
+++ config/activitylog.php
@@ -1,12 +1,26 @@
 <?php
 
 return [
-    'activity' => 'log',
+    
+    /*
+    | -----------------------------
+    | Control creating activity log
+    | When
+    | Property gains data or not
+    | ------------------------------------
+    */
 
-    /**
-     * Control creating activity log
-     * When
-     * Property gains data or not
-     */
-    'allow_null_properties' => false
+    'allow_null_properties' => false,
+
+    /*
+    | -------------------------------------------------------------------------------
+    | Clean activity log record
+    | before the given date bellow
+    | This will only be functional when 'php artisan clean:log' command will be added
+    | to system kurnel as schedule
+    | Minimum day value is 1
+    | ----------------------------------------------------------------------------------------------
+    */
+
+    'clean_log_before_days' => 30   // Days
 ];
--- src/Console/ClearLog.php
+++ src/Console/ClearLog.php
@@ -2,7 +2,10 @@
 
 namespace Amsrafid\ActivityLog\Console;
 
+use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class ClearLog extends Command
 {
@@ -11,7 +14,7 @@ class ClearLog extends Command
      *
      * @var string
      */
-    protected $signature = 'clear:log';
+    protected $signature = 'clear:log {--D|date=}';
 
     /**
      * The console command description.
@@ -21,12 +24,28 @@ class ClearLog extends Command
     protected $description = 'Clear activity log by given time.';
 
     /**
+     * The log configuration bag.
+     *
+     * @var array
+     */
+    protected $config = null;
+
+    /**
+     * Clean before given days
+     * 
+     * @var int
+     */
+    private $cleanLogBeforeDays = 0;
+
+    /**
      * Create a new command instance.
      *
      * @return void
      */
     public function __construct()
     {
+        $this->config = config('activitylog');
+
         parent::__construct();
     }
 
@@ -37,6 +56,31 @@ class ClearLog extends Command
      */
     public function handle()
     {
+        if (! isset($this->config['clean_log_before_days'])
+            || $this->config['clean_log_before_days'] < 1) {
+            return;
+        }
+        
+        $this->cleanLogBeforeDays = $this->config['clean_log_before_days'];
+
+        $date = $this->option('date') ?? Carbon::now()->subDays($this->cleanLogBeforeDays)->format('Y-m-d');
+
+        try {
+            DB::beginTransaction();
+
+            DB::statement("DELETE
+                FROM activity_logs
+                WHERE
+                    DATE(activity_logs.created_at) < '{$date}'
+            ");
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            Log::critical($th->getMessage());
+        }
+
         return 0;
     }
 }
