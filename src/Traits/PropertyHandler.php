b59819001eb5fe2fe68ad54f4b9be09e8f0d99a7
A. M. Sadman Rafid <amsrafid@gmail.com>
2021-03-19 21:12:01 +0600

    Change setProperty arguments name

--- src/Traits/PropertyHandler.php
+++ src/Traits/PropertyHandler.php
@@ -10,17 +10,18 @@ trait PropertyHandler
     /**
      * Set property value for update
      * 
+     * @param array $new    New Value of save query
+     * @param array $old    Old Values of data
      * @return void
      */
-    public function setProperty($attributes, $original)
+    public function setProperty($new, $old)
     {
         $oldValue = [];
-        $newValue = array_diff_assoc($attributes, $original);
+        $newValue = array_diff_assoc($new, $old);
 
-        array_map(function($key) use(&$oldValue, $original) {
-            $oldValue[$key] = $original[$key];
+        array_map(function($key) use(&$oldValue, $old) {
+            $oldValue[$key] = $old[$key];
         }, array_keys($newValue));
-
 
         $this->property = [
             'old' => $oldValue,
