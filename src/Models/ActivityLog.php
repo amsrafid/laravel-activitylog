24f5157ed4ee33b86bf306bbd590e47c2d1b6a0f
A. M. Sadman Rafid <amsrafid@gmail.com>
2021-04-08 00:43:17 +0600

    Add properties method to activity log

--- src/Models/ActivityLog.php
+++ src/Models/ActivityLog.php
@@ -21,4 +21,9 @@ class ActivityLog extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function properties()
+    {
+        return json_decode($this->properties);
+    }
 }
